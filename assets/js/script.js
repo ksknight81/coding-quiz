3ee396049483bb2725e87542b7fb518726a1aae4
kevin knight <ksknight81@gmail.com>
2022-05-31 00:26:49 -0400

    setup question section

---
+++ assets/js/script.js
@@ -0,0 +1,80 @@
+
+// // timer countdown variable
+// var timeLeft = 75;
+//     var elem = document.getElementById('countdown');
+
+//     function countdownTimer(){
+//         if (timeLeft == -1) {
+//             clearTimeout(timerId);
+//             doSomething("times up");
+//         } else {
+//             elem.innerHTML = timeLeft + 'seconds remaining';
+//             timeLeft--;
+//         }
+// }
+
+let questions = [
+    {
+        question: ""
+        choiceA:
+        choiceB:
+        choiceC:
+        choiceD:
+        correct: 
+    },
+    {
+        question: ""
+        choiceA:
+        choiceB:
+        choiceC:
+        choiceD:
+        correct: 
+    },
+    {
+        question: ""
+        choiceA:
+        choiceB:
+        choiceC:
+        choiceD:
+        correct: 
+    },
+    {
+        question: ""
+        choiceA:
+        choiceB:
+        choiceC:
+        choiceD:
+        correct: 
+    },
+    {
+        question: ""
+        choiceA:
+        choiceB:
+        choiceC:
+        choiceD:
+        correct: 
+    },
+    {
+        question: ""
+        choiceA:
+        choiceB:
+        choiceC:
+        choiceD:
+        correct: 
+    },
+    {
+        question: ""
+        choiceA:
+        choiceB:
+        choiceC:
+        choiceD:
+        correct: 
+    }, {
+        question: ""
+        choiceA:
+        choiceB:
+        choiceC:
+        choiceD:
+        correct: 
+    }
+]
